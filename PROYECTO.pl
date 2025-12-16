6d2df8b35c096d2b261bc4ffc5b6519ff2246104
marcosrodriguezropero <marcosrodriguezropero@gmail.com>
2025-12-16 03:40:05 +0100

    Improve movement validation and state transitions
    Refactor movement and state management logic for the game.

--- PROYECTO.pl
+++ PROYECTO.pl
@@ -28,7 +28,7 @@ estado_inicial(estado(d2m, [i1a, i2m, i1b], liebre)).
 mostrar_estado(estado(Liebre, Sabuesos, Turno)) :-
     format("Liebre en: ~w~n", [Liebre]),
     format("Sabuesos en: ~w~n", [Sabuesos]),
-    format("Turno: ~w~n~n", [Turno]),
+    format("Turno de: ~w~n~n", [Turno]),
     pintar_tablero(estado(Liebre, Sabuesos, Turno)).
 
 
@@ -40,11 +40,11 @@ pintar_tablero(Estado) :-
     simbolo(d1m, Estado, D1M), simbolo(d2m, Estado, D2M),
     simbolo(i1b, Estado, I1B), simbolo(mb, Estado, MB), simbolo(d1b, Estado, D1B),
 
-    format("    ~w -- ~w -- ~w~n", [I1A, MA, D1A]),
-    format("   / | \\ | / | \\~n"),
-    format(" ~w - ~w - ~w - ~w - ~w~n", [I2M, I1M, MM, D1M, D2M]),
-    format("   \\ | / | \\ | /~n"),
-    format("    ~w -- ~w -- ~w~n", [I1B, MB, D1B]).
+    format("         ~w -- ~w -- ~w~n", [I1A, MA, D1A]),
+    format("      /  |  \\ |  / | \\~n"),
+    format("     ~w - ~w -- ~w -- ~w - ~w~n", [I2M, I1M, MM, D1M, D2M]),
+    format("      \\  |  / |  \\ | /~n"),
+    format("         ~w -- ~w -- ~w~n", [I1B, MB, D1B]).
 
 simbolo(Celda, estado(Liebre, Sabuesos, _), Simbolo) :-
     ( Liebre == Celda ->
@@ -54,3 +54,66 @@ simbolo(Celda, estado(Liebre, Sabuesos, _), Simbolo) :-
     ; Simbolo = 'o' % o sin color si celda vacía
     ).
 
+pertenece(Elem, [Elem|_]).
+pertenece(Elem, [H|T]) :- pertenece(Elem, T).
+
+% Comprobamos en el movimiento que el jugador sea o liebre o sabuesos, en ambos comprobamos que el destino
+% pertenece a las posiciones a las cuales la posición inicial es adyacente, y en adición a los sabuesos,
+% comprobamos que el valor de la columna de la posición inicial sea menor que la del destino para que no retrocedan.
+movimiento(Jugador, Desde, Hacia) :- 
+    (Jugador = liebre, adyacentes(Desde, Posiciones), 
+    pertenece(Hacia, Posiciones);
+    Jugador = sabuesos, adyacentes(Desde, Posiciones), 
+    pertenece(Hacia, Posiciones),
+    nodo(Desde, Col1, _), nodo(Hacia, Col2, _), Col1 =< Col2). 
+
+
+movimiento_valido(Estado, Desde, Hacia) :- 
+    Estado = estado(Liebre, Sabuesos, Turno),
+% Caso de liebre, los movimientos que puede realizar junto con que su destino no coincida con la posición
+% de los sabuesos.
+    (Turno = liebre, Desde = Liebre,
+    movimiento(Liebre, Desde, Hacia), 
+    \+ pertenece(Hacia, Sabuesos)
+    ;
+% Caso de sabuesos, los movimientos que pueden realizar los sabuesos junto con que su destino no 
+% coincida con la posición de otros sabuesos ni con la de la liebre.
+    Turno = sabuesos, pertenece(Desde, Sabuesos), 
+    movimiento(Sabuesos, Desde, Hacia),
+    Hacia \= Liebre, \+ pertenece(Hacia, Sabuesos)).
+
+% Movemos la ficha de la liebre, como es una única posición no tiene más que el movimiento sea un nodo 
+% y actualizamos el estado con este.
+mover_ficha(Estado, Movimiento, NuevoEstado) :-
+    Estado = estado(Liebre, Sabuesos, liebre),
+    movimiento_valido(Estado, Liebre, Movimiento), 
+    NuevoEstado = estado(Movimiento, Sabuesos, liebre).
+
+% Movemos la ficha del sabueso a mover, cómo son varios, la posición en la que se encuentra el sabueso 
+% a mover la retiramos de la lista de posiciones y se actualiza con su destino con las otras igual.
+mover_ficha(Estado, (Desde, Hacia), NuevoEstado) :-
+    Estado = estado(Liebre, Sabuesos, sabuesos),
+    movimiento_valido(Estado, Desde, Hacia),
+    sustituir(Sabuesos, Desde, Hacia, NuevosSabuesos),
+    NuevoEstado = estado(Liebre, NuevosSabuesos, sabuesos).
+
+% Predicado para sustituir posiciones de la lista.
+sustituir([PosicionAQuitar|T1], PosicionAQuitar, PosicionNueva, [PosicionNueva|T1]).
+sustituir([H1|T1], PosicionAQuitar, PosicionNueva, [H1|T2]) :- 
+    sustituir(T1, PosicionAQuitar, PosicionNueva, T2).
+
+% Aplicamos el movimiento elegido, moviendo la ficha y cambiando el turno que corresponde
+aplicar_movimiento(Estado, Movimiento, NuevoEstado) :- 
+    Estado = estado(Liebre, Sabuesos, Turno),
+    mover_ficha(Estado, Movimiento, Estado2),
+    cambiar_turno(Estado2, NuevoEstado).
+
+% Predicado para cambiar el turno de un estado
+cambiar_turno(Estado, NuevoEstado) :- 
+    Estado = estado(Liebre, Sabuesos, Turno),
+    (Turno = liebre, 
+    NuevoEstado = estado(Liebre, Sabuesos, sabuesos)
+    ;
+    Turno = sabuesos,
+    NuevoEstado = estado(Liebre, Sabuesos, liebre)
+    ).
